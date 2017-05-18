77ce54562f9507a44e73ad5449326ed00bb4a6ae
Griffith Asare Awuah <griffith@dolphtech.com>
2017-05-18 17:07:23 +0100

    Update port445-killer.py

--- port445-killer.py
+++ port445-killer.py
@@ -1,7 +1,8 @@
 
 import socket
 import subprocess
-import os
+import time
+import os, re
 
 def isActive() :
     pc_name = socket.gethostname()
@@ -15,25 +16,37 @@ def isActive() :
             return False
 
 def findPID(port = 445) :
-	out_bytes = subprocess.check_output(['netstat','-ano'])
-	data = out_bytes.decode().split("\r\n\r\n").split("\r\n")
-	regex = re.compile(":445.+ (\d*)")
-	pids = []
-	for process in data :
-		if ":445" in process :
-			pid = regex.search(process)
-			pids.append(pid)
-	return True 
+    out_bytes = subprocess.check_output(['netstat','-ano'])
+    data = out_bytes.decode().split("\r\n\r\n")[1].split("\r\n")
+    regex = re.compile(":445.+ (\d*)")
+    for process in data :
+        if ":445" in process :
+            pid = regex.search(process)
+            yield pid.groups()[0]
 
 def closePort() :
-	for pid in pids :
-		try :
-		     y = os.system("taskkill /F /PID {}".format(pid))
-		    print("done")
-		    input()
-		except Exception as e :
-		    print(e)
-		    input()
+    for pid in findPID() :
+        try :
+            os.system("taskkill /F /PID {}".format(pid))
+        except Exception as e :
+            print("Port {} couldnt be killer because".format(pid))
+            print(e)
+            input()
+        
+for i in range(5) :
+    if isActive() :
+        closePort()
+        print("Trying again in 2 secounds. -- {}\n\n".format(i))
+        time.sleep(2)
+    else :
+        print("Port 445 has been closed succesfully")
+        input("Enter any key to continue ... ")
+        break
+
+if isActive() :
+    print("Port 445 ccouldnt be closed !!!")
+    input("Enter any key to continue ... ")
+        
 
 
 
